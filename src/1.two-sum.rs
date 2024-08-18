d66028841557307ed1ee6c91ea038bcf365f7da8
kaganege <kaganegeozkan@gmail.com>
2024-08-18 23:06:47 +0300

    made more readable

--- src/1.two-sum.rs
+++ src/1.two-sum.rs
@@ -9,23 +9,19 @@ use super::Solution;
 // @lc code=start
 impl Solution {
   pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let mut out = Vec::new();
-
-    'top: for (i, a) in nums.iter().enumerate() {
+    for (i, a) in nums.iter().enumerate() {
       for (j, b) in nums.iter().enumerate() {
         if i == j {
           continue;
         }
 
         if a + b == target {
-          out.push(i as i32);
-          out.push(j as i32);
-          break 'top;
+          return vec![i as i32, j as i32];
         }
       }
     }
 
-    out
+    unreachable!()
   }
 }
 // @lc code=end
